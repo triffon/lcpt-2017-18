92bcd6c1585a2b79a34968afd78026bbb56546a1
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2018-03-20 11:57:01 +0200

    Реализация на предшественик и факториел чрез наредени двойки. Реализация на Сиракузката задача с комбинатор за намиране на неподвижна точка.

--- numerals.py
+++ numerals.py
@@ -58,3 +58,28 @@ def topair(u):
 def tointpair(u):
     return u(lambda x: \
              lambda y: (toint(x),toint(y)))
+
+cp    = lambda n: ccdr(n(lambda p: ccons(cs(ccar(p)))\
+                                        (ccar(p)))\
+                        (ccons(c0)(c0)))
+cfact = lambda n: ccdr(n(lambda p: ccons(cs(ccar(p)))\
+                                        (cmult(cs(ccar(p)))(ccdr(p))))\
+                        (ccons(c0)(c1)))
+cdiv2  = lambda n: ccdr(n(lambda p:
+                           (lambda s: lambda q:
+                             ccons(s)(ceven(s)(cs(q))(q)))
+                            (cs(ccar(p)))(ccdr(p)))
+                            (ccons(c0)(c0)))
+F = lambda f: lambda n: cz(n)(c0)\
+                             (cz(cp(n))(c0)\
+                                       (ceven(n)(cs(f(cdiv2(n))))\
+                                                (cs(f(cs(cmult(c3)(n)))))))
+F_ = lambda f: lambda n: cz(n)(c0)\
+                              (cz(cp(n))(c0)\
+                                        (ceven(n)(lambda y: cs(f(cdiv2(n)))(y))\
+                                                 (lambda y: cs(f(cs(cmult(c3)(n))))(y))))
+
+Y = lambda f: (lambda x: f(x(x)))(lambda x: f(x(x)))
+Z = lambda f: (lambda x: f(lambda y: x(x)(y)))(lambda x: f(lambda y: x(x)(y)))
+
+omega = lambda x: x(x)
